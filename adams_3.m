48414b9e86e31bdeb9bacfb58d5fe0fb52bcebbf
Lucas_Vieira <31492775+Lukzera@users.noreply.github.com>
2018-01-01 15:37:17 -0200

    Update Adams_3.m para trabalho final em grupo
    Adiciona velocidade ao Adams_3 para não recalcular muitos valores.

--- adams_3.m
+++ adams_3.m
@@ -1,11 +1,15 @@
 function [y,t] = adams_3(f,t0,y,h,N)
 t=t0:h:N*h;
 y = runge_kutta_4(f,t0,y,h,2);
+k1=f(y(3),t(3));
+k2 = f(y(2),t(2));
+k3 = f(y(1),t(1));
 for i =3:N
-k1=f(y(i),t(i));
-k2 = f(y(i-1),t(i-1));
-k3 = f(y(i-2),t(i-2));
 y(i+1) = y(i) +h*(23*k1/12 -16*k2/12 +5*k3/12);
+k3 = k2;
+k2 = k1;
+k1 = f(y(i+1),t(i+1));
+
 end
 
 end
