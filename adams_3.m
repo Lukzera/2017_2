94432136388c9560ff0859516ddf84e74bea9e83
Lucas_Vieira <31492775+Lukzera@users.noreply.github.com>
2018-01-01 15:41:08 -0200

    Novo update para trabalho final
    Muda runge4 para o do pacote final

--- adams_3.m
+++ adams_3.m
@@ -1,6 +1,6 @@
-function [y,t] = adams_3(f,t0,y,h,N)
+function [y,t] = ufrgs_adams_3(f,t0,y,h,N)
 t=t0:h:N*h;
-y = runge_kutta_4(f,t0,y,h,2);
+y = ufrgs_mat_runge4(f,t0,y,h,2);
 k1=f(y(3),t(3));
 k2 = f(y(2),t(2));
 k3 = f(y(1),t(1));
